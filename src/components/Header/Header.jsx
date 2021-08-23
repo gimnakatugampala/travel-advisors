8724676943e90953983975add29205e9169920e2
Gimna Katugampala <autoservicelk@gmail.com>
2021-08-22 18:06:48 -0700

    got the autocomplete from google

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { Autocomplete } from '@react-google-maps/api'
 import { AppBar,Toolbar,Typography,InputBase,Box } from '@material-ui/core'
 import SearchIcon from '@material-ui/icons/Search'
 import useStyles from './styles'
 
 
-const Header = () => {
+const Header = ({setCoordinates}) => {
 
     const classes = useStyles()
+    const [autocomplate, setAutocomplate] = useState(null)
+
+    const onLoad = (autoC) => setAutocomplate(autoC)
+
+    const onPlaceChanged = () =>{
+        const lat = autocomplate.getPlace().geomerty.location.lat()
+        const lng = autocomplate.getPlace().geomerty.location.lng()
+
+        setCoordinates({lat,lng})
+    }
 
     return (
         <AppBar position="static">
@@ -19,14 +29,14 @@ const Header = () => {
                 <Typography variant="h6" className={classes.title}> 
                     Explore new places
                 </Typography>
-                {/* <Autocomplete> */}
+                <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
                     <div className={classes.search}>
                         <div className={classes.searchIcon}>
                             <SearchIcon/>
                         </div>
                         <InputBase placeholder="Search.." classes={{root:classes.inputRoot,input:classes.inputInput}} />
                     </div>
-                {/* </Autocomplete> */}
+                </Autocomplete>
                 </Box>
             </Toolbar>
         </AppBar>
